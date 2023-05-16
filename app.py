c8c5e56716f64cc15a310e3a76cafcf3c00bddef
kenil22 <shahkenil2000@gmail.com>
2023-05-16 09:55:34 +0530

    Calculator added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from final_predicting import get_future_price, historical_data_func
 import json
+import yfinance as yf
 
 
 app = Flask(__name__)
@@ -30,6 +31,40 @@ def process_selection():
     json_data = json.dumps(response_data)
 
     return jsonify(json_data)
+
+@app.route('/calculate_profit_loss', methods=['POST'])
+def calculate_profit_loss():
+    btc = request.form.get('btc')
+    buying_price = request.form.get('price')
+    crypto = request.form.get('crypto_selection')
+    cryptocoin_dict = {'DOGE':'DOGE-GBP', 'BNB':'BNB-GBP', 'BTC':'BTC-GBP', 'ETH':'ETH-GBP', 'USDT':'USDT-GBP'}
+
+    data = yf.download([cryptocoin_dict[crypto]], period='1mo')
+    data.drop("Adj Close", inplace=True, axis=1)
+    data.drop("Volume", inplace=True, axis=1)
+    data.drop("Open", inplace=True, axis=1)
+    data.drop("High", inplace=True, axis=1)
+    data.drop("Low", inplace=True, axis=1)
+    data.reset_index(inplace=True)
+    current_price = float(data['Close'].iloc[-1])
+
+    buying_price = float(buying_price)
+    btc = float(btc)
+    Profit = (current_price - buying_price)*btc 
+    if Profit < 0:
+       response_data={
+            'profit':Profit
+            }
+       json_data = json.dumps(response_data)
+       return jsonify(json_data)
+    else:
+        response_data={
+            'profit': abs(Profit)
+            }
+        json_data = json.dumps(response_data)
+        return jsonify(json_data)
+
+    # return True
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
--- testing.py
+++ testing.py
@@ -14,7 +14,8 @@ data.drop("High", inplace=True, axis=1)
 data.drop("Low", inplace=True, axis=1)
 data.reset_index(inplace=True)
 x = data['Close'].iloc[-1]
-
+x = float(x)
+print(type(x))
 Bitcoins = 5
 Buying_Price = 50
 Current_price = 10
@@ -61,3 +62,53 @@ else:
 #     print(sym)
 #     print(get_name(sym))
 #     print("++++++++++++++++++++++++++")
+
+
+
+
+# dataframe_with_information['Date'] = dataframe_with_information['Date'].apply(lambda x: datetime.strptime(x, "%Y-%m-%d").strftime("%m/%d"))
+
+# print(last_60_days_tail)
+# print(f"{Cryptocoin_Symbol} price for {Date} is {Result}")
+# print(dataframe_with_information.columns)
+
+# fig = go.Figure(data=go.Scatter(x=dataframe_with_information['Date'], y=dataframe_with_information['Close'], mode='lines'))
+# fig.update_layout(
+#     title='Bitcoin Closing Prices',
+#     xaxis=dict(title='Date'),
+#     yaxis=dict(title='Closing Price'),
+#     hovermode='closest',
+#     width=800,  # Convert inches to pixels (1 inch = 96 pixels)
+#     height=400
+# )
+
+# Add custom hover labels using the 'text' attribute
+# fig.update_traces(text=dataframe_with_information['Date'] + '<br>' + dataframe_with_information['Close'].astype(str),
+#                   hovertemplate='%{text}')
+
+# Save the figure as HTML file
+# fig.write_html('graph.html')
+
+
+
+# plt.figure(figsize=(10, 6)) 
+# plt.plot(dataframe_with_information['Date'], dataframe_with_information['Close'])
+# plt.title('Bitcoin Closing Prices')
+# plt.xlabel('Date')
+# plt.ylabel('Closing Price')
+# # Enable hover functionality with tooltips
+# def hover(event):
+#     if event.inaxes == plt.gca():
+#         index = int(event.xdata)
+#         date = dataframe_with_information['Date'].iloc[index]
+#         closing_price = dataframe_with_information['Close'].iloc[index]
+#         plt.gca().set_title(f"Date: {date}\nClosing Price: {closing_price}", fontsize=10)
+#         plt.gcf().canvas.draw_idle()
+
+# plt.gcf().canvas.mpl_connect('motion_notify_event', hover)
+# plt.savefig('graph.png')
+# plt.show()
+
+
+
+
